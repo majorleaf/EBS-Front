45cfbb61010507a2e73a0dbd3815bbe84d128630
Madandola james <jamesmadandola@gmail.com>
2026-08-30 17:19:53 +0100

    route correction

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 import { Layout } from './components/Layout.tsx';
 import { ProtectedRoute } from './components/ProtectedRoute.tsx';
+import { Landing } from './pages/Landing.tsx';
 import { Login } from './pages/Login.tsx';
 import { Signup } from './pages/Signup.tsx';
 import { Events } from './pages/Events.tsx';
 import { EventDetails } from './pages/EventDetails.tsx';
 import { Dashboard } from './pages/Dashboard.tsx';
-import { Admin } from './pages/Admin';
+import { Profile } from './pages/Profile.tsx';
+import { Admin } from './pages/Admin.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          {/* Landing page - actual entry point now  */}
+          <Route 
+            path="/"
+            element={
+              <Layout>
+                <Landing/>
+              </Layout>
+            }
+            />
+
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
 
-          <Route
-            path="/"
-            element={<Navigate to="/events" replace />}
-          />
 
           <Route
             path="/events"
